f682c4e95cfb3a16ce12f09e9b33de3a88e613d8
Michelle Ranly <meranly8@gmail.com>
2020-09-21 03:51:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies|
-      puts "#{holiday.to_s.split("  ").map {|x|x.capitalize}.join(" ") }: #{supplies.join(", ")}"
+      puts "#{holiday.to_s.split("").collect {|x|x.capitalize}.join(" ") }: #{supplies.join(", ")}"
     end
   end
 end
