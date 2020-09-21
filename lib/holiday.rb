d8e0300a85c72ddc3f4c8477d7d5701e4c909c26
Michelle Ranly <meranly8@gmail.com>
2020-09-21 04:14:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,8 +68,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.collect do |season, holidays|
-    holidays.collect do |supply, holiday|
+  holiday_hash.collect do |season, holidays_hash|
+    holidays_hash.collect do |supply, holiday|
       if supply.include?("BBQ")
         holiday.keys
       end
