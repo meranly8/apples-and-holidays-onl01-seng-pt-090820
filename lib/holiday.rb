2af20cef11b58c7aebd4f8fe5cca1baf30d5f5f4
Michelle Ranly <meranly8@gmail.com>
2020-09-21 03:45:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,9 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday|
+  holiday_hash.each do |season, holidays|
     puts "#{season.capitalize}:"
-    holiday.each do 
+    holidays.each do 
     
   end
 end
